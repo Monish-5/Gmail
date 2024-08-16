aeb66d8eba9f6ab21ed53cc12d29047df1e819f7
Monish <164910588+Monish-5@users.noreply.github.com>
2024-08-16 21:36:21 +0530

    Create EmailApp.java

---
+++ EmailApp.java
@@ -0,0 +1,12 @@
+package org.base;
+
+public class Emailapp {
+
+	public static void main(String[] args) {
+		Email em1 =new  Email("Monish","M");
+		
+     System.out.println(em1.showInfo());
+       
+	}
+
+}
